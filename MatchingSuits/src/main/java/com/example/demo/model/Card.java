280aa3da39b227cc7c23590e2e6a16416efb53a3
Lom Thin <sarom_thin@outlook.com>
2023-03-28 23:22:22 -0700

    Completed the Card class

--- MatchingSuits/src/main/java/com/example/demo/model/Card.java
+++ MatchingSuits/src/main/java/com/example/demo/model/Card.java
@@ -1,5 +1,41 @@
 package com.example.demo.model;
 
 public class Card {
-
+	public enum Suit {
+		HEARTS('H'),
+		DIAMONDS('D'),
+		CLUBS('C'),
+		SPADES('S');
+		
+		private final char value;
+		
+		Suit(char value) {
+			this.value = value;
+		}
+		
+		public char getValue() {
+			return value;
+		}
+	}
+	
+	private final Suit suit;
+	private final char rank;
+	
+	public Card(Suit suit, char rank) {
+		this.suit = suit;
+		this.rank = rank;
+	}
+	
+	public Suit getSuit() {
+		return this.suit;
+	}
+	
+	public char getChar() {
+		return this.rank;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%c%c", rank, suit.getValue());
+	}
 }
